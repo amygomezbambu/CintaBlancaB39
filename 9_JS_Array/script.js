19fa436730a26a8a552173079b05dfde652cb503
Amy <amy@bambucode.com>
2020-04-09 19:40:21 -0600

    Se incluye código de clase 9

---
+++ 9_JS_Array/script.js
@@ -0,0 +1,31 @@
+alert("Funciona");
+var nombre = "Amy";
+var edad = "35";
+var mayorDeEdad = true;
+
+//Variables Compuestas
+//Arreglos -> Serie de datos ordenados
+
+var paises = ["Colombia", "Mexico", "Costa Rica", "El salvador"];
+
+console.log(paises);
+console.log(paises[2]);
+console.log(paises[4]);
+
+paises[4] = "España";
+
+for (i = 0; i <= 4; i++)
+
+    console.log(paises[i]);
+
+
+
+//Ejercicio de campus de pares
+
+var numeros = [];
+for (var i = 0; i < = 100 ; i++) {
+    numeros[i] = i;
+
+
+}
+
