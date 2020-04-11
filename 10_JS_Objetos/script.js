c5087779c9aaa632867166886fa8cd2d38a64ab6
Amy <amy@bambucode.com>
2020-04-10 21:14:07 -0600

    Se incluye clase 10 de JS objetos.

---
+++ 10_JS_Objetos/script.js
@@ -0,0 +1,12 @@
+var arreglo  = [1,3,5,8]
+
+//los objetos no tienen orden 
+// llave: valor
+var objetos = {
+    nombre: "La noche estrellada",
+    autor : "Van Gogh",
+    tecnica : "Oleo",
+    colores : ["#FFFFFF", "#000000"],
+    anio: 1895, 
+
+}
