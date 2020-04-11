52db5866eedb11c8c71e6080d046eb534df88d48
Amy <amy@bambucode.com>
2020-04-10 21:41:33 -0600

    Se incluyen los console de las funciones obtenerinfo

--- 10_JS_Objetos/script.js
+++ 10_JS_Objetos/script.js
@@ -1,12 +1,24 @@
-var arreglo  = [1,3,5,8]
+var arreglo = [1, 3, 5, 8]
 
 //los objetos no tienen orden 
 // llave: valor
-var objetos = {
+var objeto = {
     nombre: "La noche estrellada",
-    autor : "Van Gogh",
-    tecnica : "Oleo",
-    colores : ["#FFFFFF", "#000000"],
-    anio: 1895, 
+    autor: "Van Gogh",
+    tecnica: "Oleo",
+    colores: ["#FFFFFF", "#000000"],
+    anio: 1895,
 
+
+
+    //metodos
+    calcularEdad; function() { return 2020 - this.anio }
 }
+console.log(objeto);
+console.log(objeto.autor);
+console.log(objeto.calcularEdad());
+
+ontenerinfo: function() {
+    return "La pintura" + this.nombre + "fue pintada por" + this.autor + "fue pintada en el año" + this.anio;
+}
+console.log(objeto.ontenerinfo());
