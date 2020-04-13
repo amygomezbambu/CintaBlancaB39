2d4b1e52faab7a774e85a5930a5bb58aa5b6f854
Amy <amy@bambucode.com>
2020-04-13 02:34:08 -0600

    Se incluyen los dos cuadros adicionales del ejercicio de objetos

--- 10_JS_Objetos/script.js
+++ 10_JS_Objetos/script.js
@@ -4,7 +4,7 @@ var arreglo = [1, 3, 5, 8];
 // llave: valor
 var objeto = {
     nombre: "La noche estrellada",
-    autor: "Van Gogh",
+    autor: "Vincent Van Gogh",
     tecnica: "Oleo",
     colores: ["#FFFFFF", "#000000"],
     anio: 1895,
@@ -36,4 +36,48 @@ objetoUrl.src = objeto.url;
     console.log(objetoNombre.innerHTML)
 
 
+    /*/ Objeto pintura 2 /*/
+    var objeto2={
+    nombre: "Girasoles",
+    autor: "Vincent Van Gogh",
+    tecnica: "Oleo",
+    colores: ["#FFEC33", "#88603B"],
+    anio: 1888,
+    url: "https://previews.123rf.com/images/afonskaya/afonskaya1410/afonskaya141000023/32979985-ilustraci%C3%B3n-pintada-a-mano-pintura-al-%C3%B3leo-ramo-de-girasoles-.jpg",
 
+infoGirasoles: function() {
+return "La obtra: " + this.nombre + " fue pintada por " + this.autor + "en el año " + this.anio;
+}
+
+}
+var objeto2Nombre = document.getElementById ("objeto2-nombre");
+var objeto2Mensaje = document.getElementById ("objeto2-mensaje");
+var objeto2Url = document.getElementById ("objeto2-imagen");
+
+objeto2Nombre.innerHTML = objeto2.nombre;
+objeto2Mensaje.innerHTML = objeto2.infoGirasoles();
+objeto2Url.src = objeto2.url;
+
+
+    /*/ Objeto pintura 3 /*/
+    var objeto3={
+        nombre: "Autoretrato",
+        autor: "Vincent Van Gogh",
+        tecnica: "Oleo",
+        colores: ["#FFEC33", "#88603B"],
+        anio: 1890,
+        url: "https://painting-planet.com/images2/autorretrato-en-sombrero-de-fieltro-vincent-van_1.jpg",
+    
+    infoGirasoles: function() {
+    return "La obtra: " + this.nombre + " fue pintada por " + this.autor + "en el año " + this.anio;
+    }
+    
+    }
+    var objeto3Nombre = document.getElementById ("objeto3-nombre");
+    var objeto3Mensaje = document.getElementById ("objeto3-mensaje");
+    var objeto3Url = document.getElementById ("objeto3-imagen");
+    
+    objeto3Nombre.innerHTML = objeto3.nombre;
+    objeto3Mensaje.innerHTML = objeto3.infoGirasoles();
+    objeto3Url.src = objeto3.url;
+    
