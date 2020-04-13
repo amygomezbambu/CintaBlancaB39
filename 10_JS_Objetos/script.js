65cf6819f250039f720668ef4d20c16056119489
Amy <amy@bambucode.com>
2020-04-13 00:59:33 -0600

    Se incluye el ejercicio del cuadro starred night completo

--- 10_JS_Objetos/script.js
+++ 10_JS_Objetos/script.js
@@ -1,4 +1,4 @@
-var arreglo = [1, 3, 5, 8]
+var arreglo = [1, 3, 5, 8];
 
 //los objetos no tienen orden 
 // llave: valor
@@ -8,17 +8,32 @@ var objeto = {
     tecnica: "Oleo",
     colores: ["#FFFFFF", "#000000"],
     anio: 1895,
+    url: "https://www.salirconarte.com/wp-content/uploads/2017/06/orig_64571.jpg",
 
 
 
-    //metodos
-    calcularEdad; function() { return 2020 - this.anio }
+    // console.log(objeto);
+    // console.log(objeto.autor);
+    // console.log(objeto.calcularEdad());
+
+
+    obtenerInfo: function () {
+    return "La obra: " + this.nombre + " fue pintada por " + this.autor + " en el año " + this.anio;
+    }
+
+
 }
-console.log(objeto);
-console.log(objeto.autor);
-console.log(objeto.calcularEdad());
 
-ontenerinfo: function() {
-    return "La pintura" + this.nombre + "fue pintada por" + this.autor + "fue pintada en el año" + this.anio;
-}
-console.log(objeto.ontenerinfo());
+var objetoNombre = document.getElementById("objeto-nombre");
+var objetoMensaje = document.getElementById("objeto-mensaje");
+var objetoUrl = document.getElementById ("objeto-imagen");
+
+objetoNombre.innerHTML = objeto.nombre;
+objetoMensaje.innerHTML = objeto.obtenerInfo ();
+objetoUrl.src = objeto.url;
+
+    console.log(objeto.obtenerInfo())
+    console.log(objetoNombre.innerHTML)
+
+
+
